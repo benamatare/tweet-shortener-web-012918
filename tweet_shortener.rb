ed05f0daafa4796eb1f6b68f38e1a99e8c8e63ba
Ben Matare <ben.a.matare@gmail.com>
2018-01-27 02:42:51 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+
+def dictionary
+  words_to_be_substituted = {
+  "hello" => "hi",
+  "to" => "2",
+  "two" => "2",
+  "too" => "2",
+  "for" => "4",
+  "four" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}
+end
+
+def word_substituter string
+  holder_array = string.split(" ")
+  holder_array.collect do |word|
+    if dictionary.keys.include? word 
+      word = dictionary[word]
+    else
+      word 
+    end
+  end.join(" ")
+end
