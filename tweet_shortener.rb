cda0d44db8e29f034314a60003a2079520ebf44e
Ben Matare <ben.a.matare@gmail.com>
2018-01-27 02:43:03 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -24,3 +24,10 @@ def word_substituter string
     end
   end.join(" ")
 end
+
+def bulk_tweet_shortener array
+  bulked_tweets =array.collect do |element|
+     word_substituter element
+  end.join(" ")
+  puts bulked_tweets
+end
